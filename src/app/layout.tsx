22b0d92c7162c831cca05f944aad275307c882dd
Osman Emre Gundogdu <emreguendogdu@yandex.com>
2024-03-12 08:40:16 +0300

    refactor: remove variable usage in className of body tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next"
 import "./globals.css"
 import Navbar from "@/components/Navbar/Navbar"
-import { manrope, dance, rumeur } from "@/utils/fonts"
 
 export const metadata: Metadata = {
   title: "Decode URSELF",
@@ -16,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${manrope.variable} ${dance.variable} ${rumeur.variable} font-manrope font-light`}
+        className={`font-manrope font-light`}
       >
         <Navbar />
         {children}
